755724900914b9415ba679d142c1e70e71e4027c
Yuttasin-Phokaew <71009316+Yuttasin-Phokaew@users.noreply.github.com>
2024-02-21 15:02:37 +0700

    create header navbar

---
+++ airbnb/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './assets/components/Header/header'
+// import Search from './assets/components/Search/search'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      {/* <Search /> */}
+    </>
+  )
+}
+
+export default App
